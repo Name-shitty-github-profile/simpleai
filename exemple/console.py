4f06a688fa18b74f94c34969e115c42d1d183208
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-02-23 10:32:59 -0500

    Update console.py
    New modal so new example

--- exemple/console.py
+++ exemple/console.py
@@ -1,8 +1,6 @@
 # V1
 import simpleai, os
-key = os.environ["key"]
-ctx = simpleai.context("You are a server in a bar")
+ai = simpleai.ai("You are a server in a bar", [os.environ["key"]])
 while True:
   content = input("Human :")
-  ctn, ctx = simpleai.get(content, "Human", ctx, key)
-  print(ctn)
+  print(ai.talk(content, "Human"))
