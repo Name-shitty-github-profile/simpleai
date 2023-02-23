7dc34753d58e33e1648018bef4b157219d44b51d
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-02-23 09:56:36 -0500

    Create get.py

---
+++ src/get.py
@@ -0,0 +1,22 @@
+import openai
+from .context import context
+
+def get(prompt: str, username: str, ctx: context, key: str) -> str:
+  openai.api_key = key
+  ctx.add(username, prompt)
+  try:
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=str(ctx) + '\nAI : ',
+      temperature=0.5,
+      max_tokens=1085,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )["choices"][0]['text']
+  except Exception as e:
+    print(e)
+    ctx.delete(-1)
+    return get(prompt, username, ctx, key)
+  ctx.add("AI", prompt)
+  return response
