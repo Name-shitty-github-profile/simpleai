1d5600daec568f80b20dcd09a1ffae107eeb68fa
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-02-23 09:56:49 -0500

    Create context.py

---
+++ src/context.py
@@ -0,0 +1,20 @@
+class context:
+  def __init__(self, base: str):
+    self.base = base
+    self.lst = [base]
+
+  def delete(self, num):
+    del self.lst[-1]
+    return self
+  
+  def add(self, author: str, text: str):
+    self.lst.append(f"{author} : {text}")
+    return self
+
+  def __str__(self):
+    if len(self.lst) == 5:
+      del self.lst[1]
+    ctx = "\n".join(self.lst)
+    if len(ctx) > 8000:
+      self.lst = [self.base]
+    return ctx
