8778ae90d2bfbc4a48bda8a2743f7350f58c37a3
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-02-23 10:16:56 -0500

    Update console.py

--- exemple/console.py
+++ exemple/console.py
@@ -1,6 +1,8 @@
+# V1
 import simpleai, os
 key = os.environ["key"]
 ctx = simpleai.context("You are a server in a bar")
 while True:
   content = input("Human :")
-  print(simpleai.get(content, "Human", ctx, key))
+  ctn, ctx = simpleai.get(content, "Human", ctx, key)
+  print(ctn)
