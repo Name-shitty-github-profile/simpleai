337bdfd458df93e331a490f2bbd11ae77ca005a7
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-02-23 10:42:05 -0500

    Update ai.py

--- src/ai.py
+++ src/ai.py
@@ -2,11 +2,37 @@ from .context import context
 import openai, random
 class ai:
   def __init__(self, base: str, keys: list):
+    """
+    This class is a really simple class that will allows you to chat with an IA really easily
+    Here's an example of code to chat with the AI in the console
+
+    ```python
+    import simpleai, os
+    ai = simpleai.ai("You are a server in a bar", [os.environ["key"]])
+    while True:
+      content = input("Human :")
+      print(ai.talk(content, "Human"))
+    ```
+    """
     self.keys = keys
     self.base = base
     self.ctx = context(base)
 
   def talk(self, content: str, username: str) -> str:
+    """
+    This function is for talking with the AI
+    The first argument is for the content that you wanna send
+    The second argument is the name of the person that says that
+    Example
+    ```python
+    content = MyAi.talk("Hello!", "Angelica")
+    print(content)
+    ```
+    Console
+    ```
+    Hello, how are you?
+    ```
+    """
     openai.api_key = random.choice(self.keys)
     self.ctx.add(username, content)
     try:
